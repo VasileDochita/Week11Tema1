01eedab5e366b8a523b71a912ed6201eb94467c0
VasileDochita <54108503+VasileDochita@users.noreply.github.com>
2020-01-07 13:19:31 +0200

    Add files via upload

---
+++ TestGoatLatin/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestGoatLatin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestGoatLatin")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63fac590-2e60-4b9a-9ca2-d1cefc8a6232")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestGoatLatin/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week11Tema1;
+
+
+namespace TestGoatLatin
+{
+    public class GoatLatinTest
+    {
+        private GoatLatinClass goatLatin;
+
+
+        [TestInitialize]
+        private void Setup()
+        {
+            goatLatin = new GoatLatinClass();
+        }
+        [TestMethod]
+        [DataRow("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa")]
+        [DataRow("The quick brown fox jumped over the lazy dog", "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa")]
+
+        public void testGoatLatinValidText(string input, string expected)
+        {
+            //Arrange
+
+            //Act
+            string actual = goatLatin.ToString(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void test_Goat_Latin(string input, string expected)
+        {
+            //Arrange
+            string input1 = "I speak Goat Latin";
+            string expected1 = "Imaa peaksmaaa oatGmaaaa atinLmaaaaa";
+            //Act
+            string actual = goatLatin.ToString.Equals(input1);
+            //Assert
+            Assert.AreEqual(expected1, actual);
+        }
+    }
+
+    internal class GoatLatinClass
+    {
+        internal string ToString(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+   
+}
